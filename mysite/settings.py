b4ff7b1cc0cadc5110e8374778551a99405c73a5
Tobiasz123 <90067046+Weraarew123@users.noreply.github.com>
2024-11-19 11:37:59 +0100

    DebugToEnvV

--- mysite/settings.py
+++ mysite/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = []
 
